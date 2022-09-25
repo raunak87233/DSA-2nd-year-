6f453be6b314673597e5070fe3991cfcfa04918b
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2022-09-25 22:08:39 +0530

    Create question3.c

---
+++ assingment 2/question3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,g;
+    float d,e,f,h;
+    scanf ("%d%d",&a,&b);
+    scanf ("%f%f",&d,&e);
+    c=a+b;
+    g=a-b;
+    f=d+e;
+    h=d-e;
+    printf("%d %d\n%0.1f %0.1f",c,g,f,h);
+    
+	
+    
+    return 0;
+}

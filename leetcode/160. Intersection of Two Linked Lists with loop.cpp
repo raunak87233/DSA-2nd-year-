4ef27a92b38537ae584ccdb93634614801d8d6ce
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-01-13 12:35:23 +0530

    Create 160. Intersection of Two Linked Lists with loop.cpp

---
+++ leetcode/160. Intersection of Two Linked Lists with loop.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *temp;
+		while(headA != NULL){
+			temp = headB;
+			while(temp != NULL){
+				if(headA == temp){
+					return headA;
+				}
+				temp = temp -> next;
+			}
+			headA = headA -> next;
+		}
+		return NULL;
+	}
+
+};

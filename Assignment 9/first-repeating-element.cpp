ee5924fbe7833f3eeffb7807872bf86208be1864
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-01-16 23:25:50 +0530

    Create first-repeating-element.cpp

---
+++ Assignment 9/first-repeating-element.cpp
@@ -0,0 +1,15 @@
+//https://www.interviewbit.com/problems/first-repeating-element/
+
+
+int Solution::solve(vector<int> &A) {
+    map<int,int>mp;
+    for(auto i : A){
+        mp[i]++; //arr[i] = i ;
+    }
+    
+    for(int i : A){
+        if(mp[i]>1) return i;
+    }
+    
+    return -1;
+}

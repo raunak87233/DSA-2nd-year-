929f159cd0b6a9ad88897ff520801d85e8e780e2
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-01-29 23:10:30 +0530

    Create 2549. Count Distinct Numbers on Board.cpp

---
+++ leetcode/2549. Count Distinct Numbers on Board.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/count-distinct-numbers-on-board/submissions/887247091/
+
+
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if(n==1) return n;
+        return n-1;
+        
+    }
+};
